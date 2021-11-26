e9ad1acd7173165f8333360a93019b1ff22aa86d
Pedro Fideles <pedro.fideles162@gmail.com>
2021-11-26 18:23:26 -0300

    Exercícios prático 4

--- Fundamentos/Bloco4/Aula4.1/exercicio4.js
+++ Fundamentos/Bloco4/Aula4.1/exercicio4.js
@@ -1,14 +1,11 @@
-const nota = 55;
+const a = -1;
 
-if (nota >=80) {
-    console.log("Parabéns, você foi aprovada(o)!");
-}
-else if (nota < 80 && nota >= 60) {
-    console.log("Vocês está na nossa lista de espera.");
-}
-else if (nota < 60 && nota >=0) {
-    console.log("Você foi reprovada(o).");
-}
-else {
-    console.log("Nota inválida");
+if (a > 0){
+    console.log('Positivo');
+} else if (a < 0) {
+    console.log('Negativo');
+} else if (a == 0) {
+    console.log('Zero');
+} else {
+    console.log('valor inválido');
 }
