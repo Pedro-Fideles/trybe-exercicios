eeef5b2411588e52eb902a92ad9161315c3b75f5
Pedro Fideles <pedro.fideles162@gmail.com>
2021-11-29 19:13:54 -0300

    Entendendo o BlublleSort

---
+++ Fundamentos/Bloco4/Aula4.2/exercicio-pratico/BubbleSort.js
@@ -0,0 +1,13 @@
+let array = [4, 7, 5, 6, 1, 2, 3];
+
+for (let index = 1; index < array.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+      if (array[index] < array[secondIndex]) {
+        let position = array[index];
+        array[index] = array[secondIndex];
+        array[secondIndex] = position;
+      }
+    }
+  }
+
+console.log(array);
