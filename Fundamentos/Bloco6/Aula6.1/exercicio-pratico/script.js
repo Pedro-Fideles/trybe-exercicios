b9b87e21af9c01b305369c395366536677403336
Pedro Fideles <pedro.fideles162@gmail.com>
2022-01-04 19:19:04 -0300

    Exercício 1 parte 1

---
+++ Fundamentos/Bloco6/Aula6.1/exercicio-pratico/script.js
@@ -0,0 +1,118 @@
+// Variáveis e Seletores
+const estado = document.getElementById('estado');
+
+const estados = [
+    {
+        nome: 'Acre',
+        sigla: 'ac'
+    },
+    {
+        nome: 'Alagoas',
+        sigla: 'al'
+    },
+    {
+        nome: 'Amapá',
+        sigla: 'ap'
+    },
+    {
+        nome: 'Amazonas',
+        sigla: 'am'
+    },
+    {
+        nome: 'Bahia',
+        sigla: 'ba'
+    },
+    {
+        nome: 'Ceará',
+        sigla: 'ce'
+    },
+    {
+        nome: 'Espírito Santo',
+        sigla: 'es'
+    },
+    {
+        nome: 'Goiás',
+        sigla: 'go'
+    },
+    {
+        nome: 'Maranhão',
+        sigla: 'ma'
+    },
+    {
+        nome: 'Mato Grosso',
+        sigla: 'mt'
+    },
+    {
+        nome: 'Mato Grosso do Sul',
+        sigla: 'ms'
+    },
+    {
+        nome: 'Minas Gerais',
+        sigla: 'mg'
+    },
+    {
+        nome: 'Pará',
+        sigla: 'pa'
+    },
+    {
+        nome: 'Paraíba',
+        sigla: 'pb'
+    },
+    {
+        nome: 'Paraná',
+        sigla: 'pr'
+    },
+    {
+        nome: 'Pernambuco',
+        sigla: 'pe'
+    },
+    {
+        nome: 'Piauí',
+        sigla: 'pi'
+    },
+    {
+        nome: 'Rio de Janeiro',
+        sigla: 'rj'
+    },
+    {
+        nome: 'Rio Grande do Norte',
+        sigla: 'rn'
+    },
+    {
+        nome: 'Rio Grande do Sul',
+        sigla: 'rs'
+    },
+    {
+        nome: 'Rondônia',
+        sigla: 'ro'
+    },
+    {
+        nome: 'Roraima',
+        sigla: 'rr'
+    },
+    {
+        nome: 'Santa Catarina',
+        sigla: 'sc'
+    },
+    {
+        nome: 'São Paulo',
+        sigla: 'sp'
+    },
+    {
+        nome: 'Sergipe',
+        sigla: 'se'
+    },
+    {
+        nome: 'Tocantins',
+        sigla: 'to'
+    },
+    {
+        nome: 'Distrito Federal',
+        sigla: 'df'
+    },
+]
+
+// Funções
+function gerarEstados(estados) {
+    
+}
