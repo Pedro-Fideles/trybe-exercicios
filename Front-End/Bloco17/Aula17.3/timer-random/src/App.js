a3d93f2197d61af2f762755caf42f93161476528
Pedro Fideles <pedro.fideles162@gmail.vom>
2022-04-14 11:12:19 -0300

    Resolução do para fixar

---
+++ Front-End/Bloco17/Aula17.3/timer-random/src/App.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useRef, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [number, setNumber] = useState(0);
+  const [hit, setHit] = useState(false);
+
+  useEffect(() => {
+    const intervalNumber = setInterval(() => {
+      setNumber(Math.floor(Math.random() * 100 + 1));
+    }, 10000);
+
+    return () => { clearInterval(intervalNumber) }
+  }, []);
+
+  useEffect(() => {
+    const gotItRight = number % 3 === 0 || number % 5 === 0;
+    setHit(gotItRight);
+    if (gotItRight) {
+      setTimeout(() => {
+        setHit(false);
+      }, 4000);
+    }
+  }, [number]);
+
+  return (
+    <div className="App">
+      <span>{ number }</span>
+      { hit && <span>acertou</span> }
+    </div>
+  );
+}
+
+export default App;
