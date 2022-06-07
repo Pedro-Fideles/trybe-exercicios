41943c0dc2054bf2334f7b08191e5c5aecc101f1
Pedro Fideles <pedro.fideles162@gmail.vom>
2022-06-07 18:33:15 -0300

    exercicio 2

--- Back-End/22.2/exercices/ex1-2.js
+++ Back-End/22.2/exercices/ex1-2.js
@@ -1,12 +1,20 @@
 const numbers = (num1, num2, num3) => {
   const promise = new Promise((resolve, reject) => {
-    if (num1.isNaN() || num2.isNaN() || num3.isNaN())
+    if (isNaN(num1) || isNaN(num2) || isNaN(num3))
       reject(new Error("Informe apenas números."));
     
     const result = (num1 + num2) * num3;
 
-    if(result < 50) reject(new Error("Valor muito baixo."));
+    if(result < 5000) reject(new Error("Valor muito baixo."));
 
     resolve(result);
-  })
+  });
+
+  return promise;
 }
+
+const generateRandomNumber = () => Math.floor(Math.random() * 100 + 1);
+
+numbers(generateRandomNumber(), generateRandomNumber(), generateRandomNumber())
+  .then((result) => console.log(`Resultado: ${result}`))
+  .catch((error) => console.error(error.message));
